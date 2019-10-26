966e4f2876e62f337036ffe20811d406b9f5d32c
Krisztian Holub <kholub1989@gmail.com>
2019-10-26 11:34:53 -0400

    add TodoList and TodoItem Components

--- src/Todo.js
+++ src/Todo.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 class Todo extends Component {
   render() {
     return (
-      
+      <div>
+        <button>Edit</button>
+        <button>X</button>
+        <li>{this.props.task}</li>
+      </div>
     )
   }
 }
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import Todo from './Todo';
 
 class TodoList extends Component {
   constructor(props) {
     super(props);
+    this.state = {todos: [
+      {task: "Walk The Fish"},
+      {task: "Groom Chicken"}
+    ]}
   }
   
   render() {
+    const todos = this.state.todos.map(todo => {
+      return <Todo task={todo.task} />
+    })
     return (
-      
+      <div>
+        <h1>Todo List!</h1>
+        <ul>
+          {todos}
+        </ul>
+      </div>
     )
   }
 }
